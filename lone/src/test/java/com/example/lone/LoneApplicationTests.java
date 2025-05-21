574008eb5baf39d664549dc3f5ca3149ba33d5fb
RedietBT <133571293+RedietBT@users.noreply.github.com>
2025-05-21 10:41:40 +0300

    initalizing and seting up project

---
+++ lone/src/test/java/com/example/lone/LoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
