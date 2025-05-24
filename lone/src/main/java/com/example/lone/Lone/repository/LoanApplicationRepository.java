c0688e32769b43eb62eb69f33b133bb64688c599
RedietBT <133571293+RedietBT@users.noreply.github.com>
2025-05-24 17:29:53 +0300

    updated LoanApplicationRepository added findByStatus and findByUserEmail

--- lone/src/main/java/com/example/lone/Lone/repository/LoanApplicationRepository.java
+++ lone/src/main/java/com/example/lone/Lone/repository/LoanApplicationRepository.java
@@ -1,7 +1,15 @@
 package com.example.lone.Lone.repository;
 
 import com.example.lone.Lone.model.LoanApplication;
+import com.example.lone.Lone.model.LoanStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Long> {
+
+    List<LoanApplication> findByStatus(LoanStatus status);
+    List<LoanApplication> findByUserEmail(String email);
 }
