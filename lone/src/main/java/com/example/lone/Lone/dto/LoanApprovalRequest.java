a383b6f9a0bbc6ef8a8aa25762baa0554d150249
RedietBT <133571293+RedietBT@users.noreply.github.com>
2025-05-24 19:02:15 +0300

    created a LoanApprovalRequest DTO

---
+++ lone/src/main/java/com/example/lone/Lone/dto/LoanApprovalRequest.java
@@ -0,0 +1,22 @@
+package com.example.lone.Lone.dto;
+
+import com.example.lone.Lone.model.LoanStatus;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoanApprovalRequest {
+
+    @NotNull(message = "Loan status (APPROVED/REJECTED) is required")
+    private LoanStatus status;
+
+    @Size(max = 500, message = "Remarks cannot exceed 500 characters")
+    private String remarks;
+}
